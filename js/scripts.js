44375c8699fd6fdb45b11fec33452c18067be1cd
GiuseppeF11 <faillagiuseppe13@gmail.com>
2024-01-10 17:27:55 +0100

    Eseguito esercizio

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,24 @@ const {createApp} = Vue;
 createApp ({
     data() {
         return {
-            
+            // Creo un array vuoto
+            emails:[]
         };
+    },
+    mounted() {
+        // Creo un ciclo per generare 10 email
+        for (let i = 0; i < 10; i++) {
+            // Richiamo Axios
+            axios
+                // Con GET richiamo un API che genera email casuali
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                //
+                .then((email) => {
+                    console.log(email.data.response);
+                    // Metto l'email creata nell'array
+                    this.emails.push(email.data.response);        
+                });
+        }
     }
     // Monto l'istanza di Vue in pagina
 }).mount('#app');
