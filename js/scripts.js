dec02a72aea027a7ff74aa8b4efedebad7d5602d
GiuseppeF11 <faillagiuseppe13@gmail.com>
2024-01-10 17:44:49 +0100

    Corretto commento .then

--- js/scripts.js
+++ js/scripts.js
@@ -14,13 +14,13 @@ createApp ({
         for (let i = 0; i < 10; i++) {
             // Richiamo Axios
             axios
-                // Con GET richiamo un API che genera email casuali
+                // La funzione GET richiede un API che genera email casuali
                 .get('https://flynn.boolean.careers/exercises/api/random/mail')
-                // Come risposta dammi la funzione email
-                .then((email) => {
-                    console.log(email.data.response);
+                // La funzione THEN (con argomento 'risposta') restituisce la risposta specifica per la richiesta (la response dentro data è l'email casuale)
+                .then((risposta) => {
+                    console.log(risposta.data.response);
                     // Metto l'email creata nell'array
-                    this.emails.push(email.data.response);        
+                    this.emails.push(risposta.data.response);        
                 });
         }
     }
